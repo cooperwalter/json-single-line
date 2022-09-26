104c1dde1d6dd0eceb1dcee14f6a5b800ac9f1fb
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2022-09-25 18:12:43 -0700

    add circleci config and deploy script

---
+++ scripts/deploy_app.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+########################################
+# Put this on a Server
+# run chmod +x deploy_app.sh to make the script executable
+# 
+# Execute this script:  ./deploy_app.sh $DOCKERHUB_USERNAME/$IMAGE_NAME:$TAG
+# Replace the $TAG with the actual Build Tag you want to deploy
+#
+########################################
+
+set -e
+
+DOCKER_IMAGE=$1
+CONAINER_NAME="json-single-line-converter-container"
+
+# Check for arguments
+if [[ $# -lt 1 ]] ; then
+    echo '[ERROR] You must supply a Docker Image to pull'
+    exit 1
+fi
+
+echo "Deploying $CONTAINER_NAME to Docker Container"
+
+#Check for running container & stop it before starting a new one
+if [ $(docker inspect -f '{{.State.Running}}' $CONAINER_NAME) = "true" ]; then
+    docker stop hello_world
+fi
+
+echo "Starting $CONTAINER_NAME using Docker Image name: $DOCKER_IMAGE"
+
+docker run -d --rm=true -p 80:80  --name $CONATINER_NAME $DOCKER_IMAGE
+
+docker ps -a
