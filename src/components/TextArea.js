529a2d2870c5532e0a8c64ed24dcdafb3e6b1637
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2022-10-01 13:13:14 -0700

    add white border on focus

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -4,7 +4,7 @@ import * as styles from "../styles";
 const TextArea = ({ class: className, ...rest }) => {
   return (
     <TextareaAutosize
-      class={`text-white bg-black/50 rounded-xl p-3 font-sans border-white/70 border-2 hover:border-white text-xl outline-none ${styles.scrollbarClasses} ${className}`}
+      class={`text-white bg-black/50 rounded-xl p-3 font-sans border-white/70 border-2 hover:border-white focus:border-white text-xl outline-none ${styles.scrollbarClasses} ${className}`}
       {...rest}
     />
   );
